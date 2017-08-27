650a60910647b1158688a7e932e2f06d8dcbe7ef
andforce <86118@163.com>
2017-08-27 18:09:06 +0800

    add changepage view

---
+++ ChangePageView.m
@@ -0,0 +1,21 @@
+//
+//  ChangePageView.m
+//  Forum
+//
+//  Created by 迪远 王 on 2017/8/27.
+//  Copyright © 2017年 andforce. All rights reserved.
+//
+
+#import "ChangePageView.h"
+
+@implementation ChangePageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
--- ForumWebViewController.m
+++ ForumWebViewController.m
@@ -143,9 +143,54 @@
     }
 }
 
+- (void)hidesTabBar:(BOOL)hidden{
+
+      [UIView beginAnimations:nil context:NULL];
+
+      [UIView setAnimationDuration:0];
+
+      for (UIView *view in self.tabBarController.view.subviews) {
+
+            if ([view isKindOfClass:[UITabBar class]]) {
+
+                  if (hidden) {
+
+                        [view setFrame:CGRectMake(view.frame.origin.x, [UIScreen mainScreen].bounds.size.height, view.frame.size.width , view.frame.size.height)];
+
+                      } else {
+
+                        [view setFrame:CGRectMake(view.frame.origin.x, [UIScreen mainScreen].bounds.size.height - 49, view.frame.size.width, view.frame.size.height)];
+
+                      }
+
+                } else {
+
+                  if([view isKindOfClass:NSClassFromString(@"UITransitionView")]){
+
+                        if (hidden) {
+
+                             [view setFrame:CGRectMake(view.frame.origin.x, view.frame.origin.y, view.frame.size.width, [UIScreen mainScreen].bounds.size.height)];
+
+                            } else {
+
+                              [view setFrame:CGRectMake(view.frame.origin.x, view.frame.origin.y, view.frame.size.width, [UIScreen mainScreen].bounds.size.height - 49 )];
+
+                            }
+
+                      }
+
+                }
+
+          }
+
+      [UIView commitAnimations];
+
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    [self hidesTabBar:YES];
 
     pageDic = [NSMutableDictionary dictionary];
 
