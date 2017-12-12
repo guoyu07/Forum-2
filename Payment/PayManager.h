ddcc071ab99f855ed1163cdd0a3e4f52beeb6cfc
andforce <86118@163.com>
2017-12-12 18:17:58 +0800

    update

---
+++ Payment/PayManager.h
@@ -0,0 +1,17 @@
+//
+// Created by WDY on 2017/12/12.
+// Copyright (c) 2017 andforce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PayManager : NSObject
+
++(instancetype) shareInstance;
+
+-(void) payForProductID:(NSString *)productID;
+
+- (void)dealloc;
+
+@end
--- SupportForums/SupportForumTableViewController.m
+++ SupportForums/SupportForumTableViewController.m
@@ -13,11 +13,9 @@
 #import "SupportForums.h"
 #import "AppDelegate.h"
 #import "LocalForumApi.h"
-#import <StoreKit/StoreKit.h>
 
-@interface SupportForumTableViewController ()<CAAnimationDelegate,SKPaymentTransactionObserver, SKProductsRequestDelegate>{
+@interface SupportForumTableViewController ()<CAAnimationDelegate>{
     
-    NSString * _currentProId;
 }
 
 @end
@@ -44,8 +42,6 @@
         self.navigationItem.leftBarButtonItem.title = @"";
     }
 
-    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
-    _currentProId = @"ForumForYear";
 }
 
 - (BOOL)setPullRefresh:(BOOL)enable {
@@ -114,48 +110,30 @@
     return [[[LocalForumApi alloc] init] isHaveLogin:host];
 }
 
-- (void)requestProductData:(NSString *)type{
-    NSLog(@"-------------请求对应的产品信息----------------");
-    
-//    [SVProgressHUD showWithStatus:nil maskType:SVProgressHUDMaskTypeBlack];
-    
-    NSArray *product = [[NSArray alloc] initWithObjects:type,nil];
-    
-    NSSet *nsset = [NSSet setWithArray:product];
-    SKProductsRequest *request = [[SKProductsRequest alloc] initWithProductIdentifiers:nsset];
-    request.delegate = self;
-    [request start];
-    
-}
-
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 
-    if ([SKPaymentQueue canMakePayments]) {
-        [self requestProductData:@"ForumForYear"];
-    } else {
-        NSLog(@"应用没有开启内购权限");
-    }
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
     
-//    [tableView deselectRowAtIndexPath:indexPath animated:NO];
-//    Forums *forums = self.dataList[(NSUInteger) indexPath.row];
-//
-//    NSURL * url = [NSURL URLWithString:forums.url];
-//
-//    LocalForumApi *localForumApi = [[LocalForumApi alloc] init];
-//    [localForumApi saveCurrentForumURL:forums.url];
-//
-//    if ([self isUserHasLogin:url.host]) {
-//
-//        UIStoryboard *stortboard = [UIStoryboard mainStoryboard];
-//        [stortboard changeRootViewControllerTo:@"ForumTabBarControllerId"];
-//
-//    } else{
-//
-//        id<ForumConfigDelegate> forumConfig = [ForumApiHelper forumConfig:localForumApi.currentForumHost];
-//
-//        NSString * cId = forumConfig.loginControllerId;
-//        [[UIStoryboard mainStoryboard] changeRootViewControllerTo:cId withAnim:UIViewAnimationOptionTransitionFlipFromTop];
-//    }
+
+    Forums *forums = self.dataList[(NSUInteger) indexPath.row];
+
+    NSURL * url = [NSURL URLWithString:forums.url];
+
+    LocalForumApi *localForumApi = [[LocalForumApi alloc] init];
+    [localForumApi saveCurrentForumURL:forums.url];
+
+    if ([self isUserHasLogin:url.host]) {
+
+        UIStoryboard *stortboard = [UIStoryboard mainStoryboard];
+        [stortboard changeRootViewControllerTo:@"ForumTabBarControllerId"];
+
+    } else{
+
+        id<ForumConfigDelegate> forumConfig = [ForumApiHelper forumConfig:localForumApi.currentForumHost];
+
+        NSString * cId = forumConfig.loginControllerId;
+        [[UIStoryboard mainStoryboard] changeRootViewControllerTo:cId withAnim:UIViewAnimationOptionTransitionFlipFromTop];
+    }
 
 }
 
@@ -211,141 +189,6 @@
 - (UIStatusBarStyle)preferredStatusBarStyle {
     return UIStatusBarStyleDefault;
 }
-
-// request Failed
-- (void)request:(SKRequest *)request didFailWithError:(NSError *)error{
-    
-}
-
-// request Response
-- (void)productsRequest:(nonnull SKProductsRequest *)request didReceiveResponse:(nonnull SKProductsResponse *)response {
-    NSLog(@"--------------收到产品反馈消息---------------------");
-    NSArray *product = response.products;
-    if([product count] == 0){
-        //[SVProgressHUD dismiss];
-        NSLog(@"--------------没有商品------------------");
-        return;
-    }
-    
-    NSLog(@"productID:%@", response.invalidProductIdentifiers);
-    NSLog(@"产品付费数量:%lu",(unsigned long)[product count]);
-    
-    SKProduct *p = nil;
-    for (SKProduct *pro in product) {
-        NSLog(@"%@", [pro description]);
-        NSLog(@"%@", [pro localizedTitle]);
-        NSLog(@"%@", [pro localizedDescription]);
-        NSLog(@"%@", [pro price]);
-        NSLog(@"%@", [pro productIdentifier]);
-        
-        if([pro.productIdentifier isEqualToString:_currentProId]){
-            p = pro;
-        }
-    }
-    
-    SKPayment *payment = [SKPayment paymentWithProduct:p];
-    
-    NSLog(@"发送购买请求");
-    [[SKPaymentQueue defaultQueue] addPayment:payment];
-}
-
-// remove all payment queue
-- (void)dealloc{
-    [[SKPaymentQueue defaultQueue] removeTransactionObserver:self];
-}
-
-// payment result
-- (void)paymentQueue:(nonnull SKPaymentQueue *)queue updatedTransactions:(nonnull NSArray<SKPaymentTransaction *> *)transactions {
-    for(SKPaymentTransaction *tran in transactions){
-        switch (tran.transactionState) {
-            case SKPaymentTransactionStatePurchased:{
-                NSLog(@"交易完成");
-                // 发送到苹果服务器验证凭证
-                [self verifyPurchaseWithPaymentTransaction];
-                [[SKPaymentQueue defaultQueue] finishTransaction:tran];
-                
-            }
-                break;
-            case SKPaymentTransactionStatePurchasing:
-                NSLog(@"商品添加进列表");
-                
-                break;
-            case SKPaymentTransactionStateRestored:{
-                NSLog(@"已经购买过商品");
-                
-                [[SKPaymentQueue defaultQueue] finishTransaction:tran];
-            }
-                break;
-            case SKPaymentTransactionStateFailed:{
-                NSLog(@"交易失败");
-                [[SKPaymentQueue defaultQueue] finishTransaction:tran];
-                //[SVProgressHUD showErrorWithStatus:@"购买失败"];
-            }
-                break;
-            default:
-                break;
-        }
-    }
-}
-
-////交易结束
-//- (void)completeTransaction:(SKPaymentTransaction *)transaction{
-//    NSLog(@"交易结束");
-//    [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-//}
-
-//沙盒测试环境验证
-#define SANDBOX @"https://sandbox.itunes.apple.com/verifyReceipt"
-//正式环境验证
-#define AppStore @"https://buy.itunes.apple.com/verifyReceipt"
-/**
- *  验证购买，避免越狱软件模拟苹果请求达到非法购买问题
- *
- */
--(void)verifyPurchaseWithPaymentTransaction{
-    //从沙盒中获取交易凭证并且拼接成请求体数据
-    NSURL *receiptUrl=[[NSBundle mainBundle] appStoreReceiptURL];
-    NSData *receiptData=[NSData dataWithContentsOfURL:receiptUrl];
-    
-    NSString *receiptString=[receiptData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];//转化为base64字符串
-    
-    NSString *bodyString = [NSString stringWithFormat:@"{\"receipt-data\" : \"%@\", \"password\":\"%@\"}", receiptString, @"b3189c215c0b423d985bc8d2548bb91a"];//拼接请求数据
-    NSData *bodyData = [bodyString dataUsingEncoding:NSUTF8StringEncoding];
-    
-    
-    //创建请求到苹果官方进行购买验证
-    NSURL *url=[NSURL URLWithString:SANDBOX];
-    NSMutableURLRequest *requestM=[NSMutableURLRequest requestWithURL:url];
-    requestM.HTTPBody=bodyData;
-    requestM.HTTPMethod=@"POST";
-    //创建连接并发送同步请求
-    NSError *error=nil;
-    NSData *responseData=[NSURLConnection sendSynchronousRequest:requestM returningResponse:nil error:&error];
-    if (error) {
-        NSLog(@"验证购买过程中发生错误，错误信息：%@",error.localizedDescription);
-        return;
-    }
-    NSDictionary *dic=[NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingAllowFragments error:nil];
-    NSLog(@"%@",dic);
-    if([dic[@"status"] intValue]==0){
-        NSLog(@"购买成功！");
-        NSDictionary *dicReceipt= dic[@"receipt"];
-        NSDictionary *dicInApp=[dicReceipt[@"in_app"] firstObject];
-        NSString *productIdentifier= dicInApp[@"product_id"];//读取产品标识
-        //如果是消耗品则记录购买数量，非消耗品则记录是否购买过
-        NSUserDefaults *defaults=[NSUserDefaults standardUserDefaults];
-        if ([productIdentifier isEqualToString:@"123"]) {
-            int purchasedCount=[defaults integerForKey:productIdentifier];//已购买数量
-            [[NSUserDefaults standardUserDefaults] setInteger:(purchasedCount+1) forKey:productIdentifier];
-        }else{
-            [defaults setBool:YES forKey:productIdentifier];
-        }
-        //在此处对购买记录进行存储，可以存储到开发商的服务器端
-    }else{
-        NSLog(@"购买失败，未通过验证！");
-    }
-}
-
 
 @end
 
